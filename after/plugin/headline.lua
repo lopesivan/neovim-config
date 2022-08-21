6b4ecf698b72519e0914cd182e2cd30f8c18e318
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-08-21 03:30:00 -0300

    Atualiza o arquivo after/plugin/headline.lua

--- after/plugin/headline.lua
+++ after/plugin/headline.lua
@@ -2,10 +2,14 @@
 -- Maintainer: Ivan Lopes
 -- ~/.config/nvim/after/plugin
 
+-- Descobrir regiao.
+--echo map(synstack(line('.'), col('.')), 'synIDattr(v:val, "name")')
+
 vim.cmd [[highlight Headline1 guibg=#1e2718]]
 vim.cmd [[highlight Headline2 guibg=#21262d]]
 vim.cmd [[highlight CodeBlock guibg=#1c1c1c]]
 vim.cmd [[highlight Dash guibg=#D19A66 gui=bold]]
+vim.cmd [[highlight markdowncodeblock guibg=#1c1c1c]]
 
 vim.fn.sign_define("Headline1", { linehl = "Headline1" })
 vim.fn.sign_define("Headline2", { linehl = "Headline2" })
@@ -39,6 +43,5 @@ require("headlines").setup {
         dash_highlight = "Dash",
     },
 }
-
 
 -- vim: fdm=marker:sw=4:sts=4:et
